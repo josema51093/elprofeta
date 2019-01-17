b748656f3d4347bbd43702f08061673febf92e5d
Josema Perez Castro <josema51093@gmail.com>
2019-01-17 15:23:12 +0100

    eliminacion archivos innecesarios y adaptado breakpoint

---
+++ js/funciones.js
@@ -0,0 +1,77 @@
+function mostrarOcultar(){
+			var statusDisplay = document.getElementById("mostrar").style.display;
+			var valueEnlace = document.getElementById("noticia").value;
+
+			if (statusDisplay == "block") {
+				document.getElementById("noticia").innerHTML = 'Leer más';
+				document.getElementById("mostrar").style.display='none';	
+			} else {
+				document.getElementById("noticia").innerHTML = 'Cerrar';
+				document.getElementById("mostrar").style.display='block';
+			}
+		}
+
+		function mostrarOcultar2(){
+			var statusDisplay = document.getElementById("mostrar2").style.display;
+			var valueEnlace = document.getElementById("noticia2").value;
+
+			if (statusDisplay == "block") {
+				document.getElementById("noticia2").innerHTML = 'Leer más';
+				document.getElementById("mostrar2").style.display='none';	
+			} else {
+				document.getElementById("noticia2").innerHTML = 'Cerrar';
+				document.getElementById("mostrar2").style.display='block';
+			}
+		}
+
+		function mostrarOcultar3(){
+			var statusDisplay = document.getElementById("mostrar3").style.display;
+			var valueEnlace = document.getElementById("noticia3").value;
+
+			if (statusDisplay == "block") {
+				document.getElementById("noticia3").innerHTML = 'Leer más';
+				document.getElementById("mostrar3").style.display='none';	
+			} else {
+				document.getElementById("noticia3").innerHTML = 'Cerrar';
+				document.getElementById("mostrar3").style.display='block';
+			}
+		}
+
+		function mostrarOcultar4(){
+			var statusDisplay = document.getElementById("mostrar4").style.display;
+			var valueEnlace = document.getElementById("noticia4").value;
+
+			if (statusDisplay == "block") {
+				document.getElementById("noticia4").innerHTML = 'Leer más';
+				document.getElementById("mostrar4").style.display='none';	
+			} else {
+				document.getElementById("noticia4").innerHTML = 'Cerrar';
+				document.getElementById("mostrar4").style.display='block';
+			}
+		}
+
+		function mostrarOcultar5(){
+			var statusDisplay = document.getElementById("mostrar5").style.display;
+			var valueEnlace = document.getElementById("noticia5").value;
+
+			if (statusDisplay == "block") {
+				document.getElementById("noticia5").innerHTML = 'Leer más';
+				document.getElementById("mostrar5").style.display='none';	
+			} else {
+				document.getElementById("noticia5").innerHTML = 'Cerrar';
+				document.getElementById("mostrar5").style.display='block';
+			}
+		}
+
+		function mostrarOcultar6(){
+			var statusDisplay = document.getElementById("mostrar6").style.display;
+			var valueEnlace = document.getElementById("noticia6").value;
+
+			if (statusDisplay == "block") {
+				document.getElementById("noticia6").innerHTML = 'Leer más';
+				document.getElementById("mostrar6").style.display='none';	
+			} else {
+				document.getElementById("noticia6").innerHTML = 'Cerrar';
+				document.getElementById("mostrar6").style.display='block';
+			}
+		}
